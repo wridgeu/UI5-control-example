b6eef80dde8195f7b32dbe2659cca5af62ef661b
Marco Beier <marco-beier@t-online.de>
2022-11-04 22:29:57 +0100

    refactor: slight adjustments to one example
    others may follow ...

--- webapp/controls/ExtendedInput.js
+++ webapp/controls/ExtendedInput.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/m/Input",
+	"sap/m/InputRenderer",
 	"sap/m/Button",
 	"sap/m/MessageToast"
-], function(Input, Button, MessageToast) {
+], function (Input, InputRenderer, Button, MessageToast) {
 	"use strict";
 
 	return Input.extend("com.mrb.customcontrol.controls.ExtendedInput", {
@@ -24,43 +25,41 @@ sap.ui.define([
 				onSelect: {
 					allowPreventDefault: true,
 					parameters: {
-						"parameterOne" : {type: "string"}
+						"parameterOne": { type: "string" }
 					}
 				},
 				press: {}
 			},
 		},
 
-		init: function() {
-			var that = this;
+		init: function () {
 			Input.prototype.init.call(this);
 			//tell attach us to the event and call our method
 			this.attachValueHelpRequest(this.onValueHelpRequest);
 			//no need for onChange to be attached here, idk why~
 			this.attachChange(this.onChange);
-			this.oButton = new Button({
+			this.setAggregation("button", new Button({
 				text: "This is an aggregation",
-				press: function(oEvt){
-					that.firePress(oEvt)
+				press: (evt) => {
+					this.firePress(evt)
 				}
-			});
-			this.setAggregation("button", this.oButton)
+			}));
 		},
 
-		onValueHelpRequest: function(){
+		onValueHelpRequest: function () {
 			MessageToast.show("onValueHelpRequest has been fired");
 			this.fireEvent("onSelect", {
-				parameterOne: "someText" 
+				parameterOne: "someText"
 			})
 		},
 		//onChange is being inherited from sap.m.InputBase
-		onChange: function() {
+		onChange: function () {
 			MessageToast.show("onChange has been fired in extended control");
 		},
-		//use standard renderer
-		renderer: function(oRm, oCustomControl){
-			sap.m.InputRenderer.render(oRm, oCustomControl);
-			oRm.renderControl(oCustomControl.getAggregation("button"))
-		} 
+
+		renderer: function (rm, control) {
+			InputRenderer.render(rm, control); // use standard renderer
+			rm.renderControl(control.getAggregation("button")) // renderer custom button aggregation
+		}
 	});
 });
